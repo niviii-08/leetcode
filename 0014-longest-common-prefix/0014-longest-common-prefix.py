8da7d94858eaf2c187db1a39c85f49a66d9c36f8
niviii-08 <neevetha2006@gmail.com>
2026-03-13 22:29:13 +0530

    Time: 2 ms (35.56%), Space: 12.5 MB (31.96%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        pre=""
+        for i in range(len(strs[0])):
+            ch=strs[0][i]
+            for j in strs:
+                if i>=len(j) or j[i]!=ch:
+                    return pre
+            pre+=ch
+        return pre
