662b9ebdbc3fedbb6c7415488dadc552ac992727
niviii-08 <neevetha2006@gmail.com>
2026-03-23 00:16:09 +0530

    Time: 0 ms (100%), Space: 12.5 MB (46.65%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        stack = []
+        parts = path.split("/")
+        for part in parts:
+            if part == "" or part == ".":
+                continue
+            elif part == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(part)
+        return "/" + "/".join(stack)
