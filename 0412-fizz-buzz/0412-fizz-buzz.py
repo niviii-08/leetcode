987168be756b8df985c4b04e978efe0811fa1073
niviii-08 <neevetha2006@gmail.com>
2026-04-22 02:06:43 +0530

    Time: 2 ms (49.82%), Space: 13.1 MB (97.3%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        list=[]
+        i=1
+        while i <= n:
+            if i%3==0 and i%5==0:
+                list.append("FizzBuzz")
+            elif i%3==0:
+                list.append("Fizz")
+            elif i%5==0:
+                list.append("Buzz")
+            else:
+                list.append(str(i))
+            i=i+1
+        return list
