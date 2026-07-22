dc6e2ed7dc86ae3dcc32b25d06e545593f412d03
niviii-08 <neevetha2006@gmail.com>
2026-07-22 22:12:02 +0530

    Time: 6 ms (61.73%), Space: 13.5 MB (76.55%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,16 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        nums1=[]
-        count=0
+        count = 0
+
         for i in range(len(nums)):
-            if nums[i]== 0:
-                count+=1
-            else:
-                nums1.append(nums[i])
-        for i in range(count):
-            nums1.append(0)
-        for i in range(len(nums)):
-            nums[i]=nums1[i]
-                
+            if nums[i] != 0:
+                nums[count] = nums[i]
+                count += 1
+
+        while count < len(nums):
+            nums[count] = 0
+            count += 1
         
