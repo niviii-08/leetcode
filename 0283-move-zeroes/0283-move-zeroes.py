dbe1e7743ac6aa6f1f0f8e0713782c2a0f10b848
niviii-08 <neevetha2006@gmail.com>
2026-07-22 22:18:53 +0530

    Time: 9 ms (32.4%), Space: 13.5 MB (48.78%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,14 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        count = 0
+        left = 0
 
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[count] = nums[i]
-                count += 1
-
-        while count < len(nums):
-            nums[count] = 0
-            count += 1
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+        
+                
         
